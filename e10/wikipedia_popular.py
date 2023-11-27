ce1a497592936959093c585e7f1fc690fbe76238
Jason Nguyen <vkn3@sfu.ca>
2023-11-27 13:20:16 -0800

    add e12

--- e10/wikipedia_popular.py
+++ e10/wikipedia_popular.py
@@ -1,11 +1,12 @@
 import sys
 from pyspark.sql import SparkSession, functions, types
+import re
 
 spark = SparkSession.builder.appName('popular wikipedia pages').getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 
-assert sys.version_info >= (3, 8) # make sure we have Python 3.8+
-assert spark.version >= '3.2' # make sure we have Spark 3.2+
+assert sys.version_info >= (3, 8)  # make sure we have Python 3.8+
+assert spark.version >= '3.2'  # make sure we have Spark 3.2+
 
 views_schema = types.StructType([
     types.StructField('language', types.StringType()),
@@ -14,10 +15,47 @@ views_schema = types.StructType([
     types.StructField('returned_bytes', types.LongType())
 ])
 
-def main(in_directory, out_directory):
-    views = spark.read.csv(in_directory, schema=views_schema).withColumn('filename', functions.input_file_name())
 
-if __name__=='__main__':
+def extract_hour_from_path(path):
+    # Assume filenames will be in the format pagecounts-YYYYMMDD-HHMMSS*
+    # Output: YYYYMMDD-HH
+    pattern = r'pagecounts-(\d{8})-(\d{2}).*'
+
+    match = re.search(pattern, path)
+    if match:
+        date = match.group(1)
+        hour = match.group(2)
+        return f"{date}-{hour}"
+    else:
+        return None
+
+
+def main(in_directory, out_directory):
+    views = spark.read.csv(in_directory, sep='', schema=views_schema).withColumn(
+        'filename', functions.input_file_name())
+
+    views = views.filter((views['language'] == 'en') &
+                         (views['title'] != 'Main_Page') &
+                         (~views['title'].startswith('Special:')))
+
+    path_to_hour = functions.udf(
+        extract_hour_from_path, returnType=types.StringType())
+
+    views = views.withColumn('date_hour', path_to_hour(views.filename))
+
+    views = views.cache()
+
+    top_views = views.groupBy('date_hour').agg(
+        functions.max('requested_times').alias('requested_times'))
+
+    views = views.join(top_views, on=['date_hour', 'requested_times'])
+    views = views.orderBy('date_hour', 'title')
+    views = views.select('date_hour', 'title', 'requested_times')
+
+    views.write.csv(out_directory, mode='overwrite')
+
+
+if __name__ == '__main__':
     in_directory = sys.argv[1]
     out_directory = sys.argv[2]
     main(in_directory, out_directory)
---
+++ e12/wordcount-1/wordcount.py
@@ -0,0 +1,32 @@
+import re
+import string
+from pyspark.sql.functions import split, explode, lower, count
+from pyspark.sql import SparkSession
+import sys
+assert sys.version_info >= (3.8)
+# regex that matches spaces and/or punctuation
+wordbreak = r'[%s\s]+' % (re.escape(string.punctuation),)
+
+
+def main(in_directory, out_directory):
+    # 1. Read lines from the files with spark.read.text
+    df = spark.read.text(in_directory)
+
+    # 2. Split the lines into words with the regex. Use the split and explode functions. Normalize all of the strings to lower-case.
+    df = df.select(split(df.value, wordbreak).alias('split'))
+    df = df.select(explode(df.split).alias('exploded'))
+    df = df.select(lower(df.exploded).alias('word'))
+
+    # 3. Count the number of times each word occurs
+    df = df.groupBy('word').agg(count(df.word).alias('word'))
+
+
+if __name__ == '__main__':
+    in_directory = sys.argv[1]
+    out_directory = sys.argv[2]
+    spark = SparkSession.builder.appName(
+        'Word Count').getOrCreate()
+    assert spark.version >= '3.2'  # make sure we have Spark 3.2+
+    spark.sparkContext.setLogLevel('WARN')
+
+    main(in_directory, out_directory)
